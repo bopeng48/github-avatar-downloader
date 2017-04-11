0814a594e9fbf9ee3963fc86528164dfebc6f0ba
Bo <bopeng48@gmail.com>
2017-04-11 05:47:53 +0000

    authericate thing added

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,15 @@
 var request = require('request');
+var CREDENTIAL = require('../cred.js');
+var GITHUB_USER = CREDENTIAL.usr;
+var GITHUB_TOKEN = CREDENTIAL.pw;
+
+console.log(GITHUB_USER,GITHUB_TOKEN);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
